458541311b6e9489b24ac23a8b944a6606157da3
SimonB <62609286+NotVeryGoodAtCodingButOk@users.noreply.github.com>
2020-09-25 10:44:40 -0500

    This is the initial commit

---
+++ Assets/Scripts/DesitionLevelManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagment;
+
+public class DesitionLevelManager : MonoBehaviour
+{
+    private float startdelay = 2.114f;
+    
+    void toNextDesition2
+    {
+        SceneManager.LoadScene(SceneManager.getActiveScene.buildIndex() + 2);
+    }
+    void toNextDesition3
+    {
+        SceneManager.LoadScene(SceneManager.getActiveScene.buildIndex() + 1);
+    }
+
+    public void executestuff1
+    {
+        Invoke("toNextDesition", startdelay);
+    }
+    public void executestuff2
+    {
+        Invoke("toNextDesition2", startdelay);
+    }
+    
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool GameHasEnded = false;
+
+    private bool isChoiceLevel = false;
+    public float RestartDelay = 2.114f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if (GameHasEnded == false)
+        {
+            GameHasEnded = true;
+            Debug.Log("GAME OVER");
+            Invoke("Restart", RestartDelay);
+        }
+
+    }
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+}
